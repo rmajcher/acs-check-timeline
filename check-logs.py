5bf2839c715f0ed741f3eac72c040d3bc985bca8
Russell Majcher <rmajcher@MBP-Russell.local>
2022-08-31 11:33:09 -0600

    adding slack

--- check-logs.py
+++ check-logs.py
@@ -1,6 +1,7 @@
 import boto3
 import argparse, sys
 import time
+import requests
 
 description = \
 """Query ACS CloudWatch Logs"""
@@ -10,6 +11,8 @@ parser.add_argument("-e", "--execution", default='local', help="determines if th
 parser.add_argument('-t', '--tier', help='specify other SecOps user tier default: ProdOpsTier4', default='ProdOpsTier4', required=False)
 parser.add_argument('-p', '--profile',help='specify AWS account: prod / preprod default: prod', default='prod', required=False)
 args=parser.parse_args()
+
+slack_message = []
 
 if args.execution == 'local':
     accounts = {
@@ -33,6 +36,15 @@ else:
         aws_secret_access_key = credentials['SecretAccessKey'],
         aws_session_token = credentials['SessionToken'],
     )
+
+def acsRestartTriageSlack(message_text):
+    url = 'https://hooks.slack.com/services/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'
+    response = requests.post(url, json = message_text, headers = {"Content-type": "application/json"})
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text)
+        )
 
 def main():
     t = time.time()
@@ -70,13 +82,25 @@ def main():
         print(f'NewTimeline:        {new_timeline_results}')
         print(f'ExpiredHandling:    {handling_results}')
         print(f'DeadlineExceeded:   {DEADLINE_EXCEEDED_results}')
+        slack_message.append('TIMELINES ARE OUT OF SYNC RESTART ACS')
+        slack_message.append(f'NewTimeline:        {new_timeline_results}')
+        slack_message.append(f'ExpiredHandling:    {handling_results}')
+        slack_message.append(f'DeadlineExceeded:   {DEADLINE_EXCEEDED_results}')
+        slack_message_json = {"text": '\n'.join([str(item) for item in slack_message])}
+        acsRestartTriageSlack(slack_message_json)
     elif DEADLINE_EXCEEDED_results != []:
         print('DEADLINE_EXCEEDED RESTART ACS')
         print(f'NewTimeline:        {new_timeline_results}')
         print(f'ExpiredHandling:    {handling_results}')
         print(f'DeadlineExceeded:   {DEADLINE_EXCEEDED_results}')
+        slack_message.append('DEADLINE_EXCEEDED RESTART ACS')
+        slack_message.append(f'NewTimeline:        {new_timeline_results}')
+        slack_message.append(f'ExpiredHandling:    {handling_results}')
+        slack_message.append(f'DeadlineExceeded:   {DEADLINE_EXCEEDED_results}')
+        slack_message_json = {"text": '\n'.join([str(item) for item in slack_message])}
+        acsRestartTriageSlack(slack_message_json)
     else:
-        print('timelines in sync')
+        print('ACS timelines in sync')
         print(f'NewTimeline:        {new_timeline_results}')
         print(f'ExpiredHandling:    {handling_results}')
         print(f'DeadlineExceeded:   {DEADLINE_EXCEEDED_results}')
